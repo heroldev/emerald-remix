066fde72809048ce2f39256057e67683992ec04d
Andrew Herold <heroldaksd@gmail.com>
2022-04-04 15:41:01 -0500

    wow, turns out semicolons are important

--- include/reset_rtc_program.h
+++ include/reset_rtc_program.h
@@ -1,6 +1,6 @@
 #ifndef GUARD_RESET_RTC_PROGRAM_H
 #define GUARD_RESET_RTC_PROGRAM_H
 
-void CB2_InitResetRtcProgram(void)
+void CB2_InitResetRtcProgram(void);
 
-#endif //GUARD_RESET_RTC_PROGRAM_H
+#endif // GUARD_RESET_RTC_PROGRAM_H
